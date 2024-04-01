28d126dd7b073f70cc9b87e991319e0e02389e2e
Danica <165607381+DanicaGrace21@users.noreply.github.com>
2024-04-01 11:51:28 +0800

    Add files via upload

---
+++ Assignment 3(ALBA).cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment_3_ALBA_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Seconds :  ");
+            string a = Console.ReadLine();
+            float X = (int)Int64.Parse(a);
+
+            float SecTMin = X / 60;
+            float MinHrs  = SecTMin / 60;
+            float HrsDay  = MinHrs / 24;
+            float DayMnth = HrsDay / 30;
+
+            Console.WriteLine("Seconds to Minutes :" + SecTMin + " Minutes");
+            Console.WriteLine("Minutes to Hours   :" + MinHrs + " Hours");
+            Console.WriteLine("Hours   to Days    :" + HrsDay + " Days");
+            Console.WriteLine("Days    to Months  :" + DayMnth + " Months");
+        }
+    }
+}
